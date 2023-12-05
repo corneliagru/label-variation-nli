8f3d78e5dc10b95a7f4f8d65acd7981c6fe16078
Cornelia Gruber <cornelia.gru@gmail.com>
2023-12-05 12:33:52 +0100

    fix figure 1

--- src/custom_funcs.py
+++ src/custom_funcs.py
@@ -5,11 +5,13 @@ import pandas as pd
 import ternary
 from sympy import symbols, solve
 
+
 def load_data_as_df(data_file):
     d_list = common.load_jsonl(data_file)
     collected_data_dict = list_dict_data_tool.list_to_dict(d_list, key_fields='uid')
     df = pd.DataFrame.from_dict(collected_data_dict, orient='index')
     return df
+
 
 class MultinomialExpectationMaximizer:
     def __init__(self, K, rtol=1e-4, max_iter=100, restarts=10):
@@ -94,8 +96,7 @@ class MultinomialExpectationMaximizer:
             loss -= np.sum(weights * np.log(weights))
         return loss
 
-
-    #initialize pi as uniform distribution, thetha (= confusion values) as dirichlet since its the cojugate prior to a multinomial
+    # initialize pi as uniform distribution, thetha (= confusion values) as dirichlet since its the cojugate prior to a multinomial
     def _init_params(self, C):
         pi = np.array([1 / self._K] * self._K)
         theta = dirichlet.rvs([2 * C] * C, self._K)
@@ -131,25 +132,26 @@ class MultinomialExpectationMaximizer:
 
         return best_loss, best_pi, best_theta, best_tau
 
-def run_em(Y, K=3):
 
+def run_em(Y, K=3):
     likelihoods = []
     best_pi = None
     best_theta = None
     best_tau = None
 
     model = MultinomialExpectationMaximizer(K, restarts=100)
-    _,pi,theta,tau = model.fit(Y)
+    _, pi, theta, tau = model.fit(Y)
     log_likelihood = model.log_lh(Y, pi, theta)
     likelihoods.append(log_likelihood)
     best_pi = pi
     best_theta = theta
     best_tau = tau
 
-    #print('best_pi: %s' % str(best_pi))
-    #print('best_theta: %s' % str(best_theta))
+    # print('best_pi: %s' % str(best_pi))
+    # print('best_theta: %s' % str(best_theta))
 
     return likelihoods, best_pi, best_theta, best_tau
+
 
 # Label matching
 # based on rel. frequency and estimated pi should be sufficient
@@ -161,7 +163,8 @@ def sort_second_array(first_array, second_array):
 
     return matched_index
 
-def tolerance_sort(array, tolerance, reverse = False):
+
+def tolerance_sort(array, tolerance, reverse=False):
     '''
     Sorts an array by the first column, but only if the difference between the rows is more than the tolerance.
     Otherwise, tolerance_sort by the next column.
@@ -185,10 +188,11 @@ def tolerance_sort(array, tolerance, reverse = False):
             array_sorted[slice(sort_range[0], sort_range[-1] +
                                1)] = sub_arr_ord
             sort_range = [i + 1]
-    if(reverse): array_sorted = np.flipud(array_sorted)
+    if (reverse): array_sorted = np.flipud(array_sorted)
 
-    matched_index = [np.where(np.all(array== x,axis=1))[0][0] for x in array_sorted]
+    matched_index = [np.where(np.all(array == x, axis=1))[0][0] for x in array_sorted]
     return array_sorted, matched_index
+
 
 def max_posterior_label(tau):
     '''
@@ -217,33 +221,37 @@ def max_posterior_label(tau):
                                                     np.where(z_int == 4, 'c', z_int)))))
     return z_hat
 
-CB_color_cycle = {0:'#377eb8', 1:'#ff7f00', 2:'#4daf4a',
-                  3:'#f781bf', 4:'#a65628', 5:'#984ea3',
-                  6:'#999999', 7:'#e41a1c', 8:'#dede00'}
-def base_plot(scale=100, fontsize=12, size=6):
+
+CB_color_cycle = {0: '#377eb8', 1: '#ff7f00', 2: '#4daf4a',
+                  3: '#f781bf', 4: '#a65628', 5: '#984ea3',
+                  6: '#999999', 7: '#e41a1c', 8: '#dede00'}
+
+
+def base_plot(scale=100, fontsize=12, size=6, multiple=10, tick_fontsize=12, tick_offset=0.02, label_offset=0):
     '''
     Generates a base ternary plot with the given scale and fontsize
     :param scale: maximum value of the plot
     :param fontsize: fontsize of the labels
     :return: figure and tax object
     '''
-    #function to generate base ternary plot
+    # function to generate base ternary plot
     figure, tax = ternary.figure(scale=scale)
     figure.set_size_inches(size, size)
     midpoint = (33.33, 33.33, 33.33)
-    tax.line((50,50,0), midpoint, color="black")
-    tax.line((0,50,50), midpoint, color="black")
-    tax.line((50,0,50), midpoint, color="black")
-    #counterclockwise drawing
-    tax.right_corner_label("entailment", fontsize=fontsize)
+    tax.line((50, 50, 0), midpoint, color="black")
+    tax.line((0, 50, 50), midpoint, color="black")
+    tax.line((50, 0, 50), midpoint, color="black")
+    # counterclockwise drawing
+    tax.right_corner_label("entailment", fontsize=fontsize, offset=label_offset)
     tax.top_corner_label("neutral", fontsize=fontsize)
-    tax.left_corner_label("contradiction", fontsize=fontsize)
+    tax.left_corner_label("contradiction", fontsize=fontsize, offset=label_offset)
     tax.boundary()
-    tax.gridlines(multiple=10)
+    tax.gridlines(multiple=multiple)
     tax.get_axes().axis('off')
-    tax.ticks(axis='lbr', linewidth=1, multiple=10)
+    tax.ticks(axis='lbr', linewidth=1, multiple=multiple, fontsize=tick_fontsize, offset=tick_offset)
     tax.clear_matplotlib_ticks()
     return figure, tax
+
 
 def create_all_labels(n_labels):
     all_labels = []
@@ -254,33 +262,39 @@ def create_all_labels(n_labels):
                     all_labels.append([i, j, k])
     return np.array(all_labels)
 
+
 def get_boundaries(pi, theta):
     y_e, y_c, y_n = symbols('y_e y_c y_n')
 
     # e-c
-    eq1 = y_e + y_c -100
+    eq1 = y_e + y_c - 100
     eq3 = y_n
-    eq2 = (np.log(pi[0]) + y_e * np.log(theta[0,0]) + y_n * np.log(theta[0,1]) + y_c * np.log(theta[0,2])) - (np.log(pi[2]) + y_e * np.log(theta[2,0]) + y_n * np.log(theta[2,1]) + y_c * np.log(theta[2,2]))
-    dict_ec = solve((eq1,eq2,eq3), (y_e,y_c,y_n))
+    eq2 = (np.log(pi[0]) + y_e * np.log(theta[0, 0]) + y_n * np.log(theta[0, 1]) + y_c * np.log(theta[0, 2])) - (
+                np.log(pi[2]) + y_e * np.log(theta[2, 0]) + y_n * np.log(theta[2, 1]) + y_c * np.log(theta[2, 2]))
+    dict_ec = solve((eq1, eq2, eq3), (y_e, y_c, y_n))
 
     # e-n
-    eq1 = y_e + y_n -100
+    eq1 = y_e + y_n - 100
     eq3 = y_c
-    eq2 = (np.log(pi[0]) + y_e * np.log(theta[0,0]) + y_n * np.log(theta[0,1]) + y_c * np.log(theta[0,2])) - (np.log(pi[1]) + y_e * np.log(theta[1,0]) + y_n * np.log(theta[1,1]) + y_c * np.log(theta[1,2]))
-    dict_en = solve((eq1,eq2,eq3), (y_e,y_c,y_n))
+    eq2 = (np.log(pi[0]) + y_e * np.log(theta[0, 0]) + y_n * np.log(theta[0, 1]) + y_c * np.log(theta[0, 2])) - (
+                np.log(pi[1]) + y_e * np.log(theta[1, 0]) + y_n * np.log(theta[1, 1]) + y_c * np.log(theta[1, 2]))
+    dict_en = solve((eq1, eq2, eq3), (y_e, y_c, y_n))
 
     # n-c
-    eq1 = y_n + y_c -100
+    eq1 = y_n + y_c - 100
     eq3 = y_e
-    eq2 = (np.log(pi[2]) + y_e * np.log(theta[2,0]) + y_n * np.log(theta[2,1]) + y_c * np.log(theta[2,2])) - (np.log(pi[1]) + y_e * np.log(theta[1,0]) + y_n * np.log(theta[1,1]) + y_c * np.log(theta[1,2]))
-    dict_nc = solve((eq1,eq2,eq3), (y_e,y_c,y_n))
+    eq2 = (np.log(pi[2]) + y_e * np.log(theta[2, 0]) + y_n * np.log(theta[2, 1]) + y_c * np.log(theta[2, 2])) - (
+                np.log(pi[1]) + y_e * np.log(theta[1, 0]) + y_n * np.log(theta[1, 1]) + y_c * np.log(theta[1, 2]))
+    dict_nc = solve((eq1, eq2, eq3), (y_e, y_c, y_n))
 
     # e-n-c
     eq1 = y_e + y_c + y_n - 100
-    eq3 = (np.log(pi[0]) + y_e * np.log(theta[0,0]) + y_n * np.log(theta[0,1]) + y_c * np.log(theta[0,2])) - (np.log(pi[1]) + y_e * np.log(theta[1,0]) + y_n * np.log(theta[1,1]) + y_c * np.log(theta[1,2]))
-    eq2 = (np.log(pi[0]) + y_e * np.log(theta[0,0]) + y_n * np.log(theta[0,1]) + y_c * np.log(theta[0,2])) - (np.log(pi[2]) + y_e * np.log(theta[2,0]) + y_n * np.log(theta[2,1]) + y_c * np.log(theta[2,2]))
+    eq3 = (np.log(pi[0]) + y_e * np.log(theta[0, 0]) + y_n * np.log(theta[0, 1]) + y_c * np.log(theta[0, 2])) - (
+                np.log(pi[1]) + y_e * np.log(theta[1, 0]) + y_n * np.log(theta[1, 1]) + y_c * np.log(theta[1, 2]))
+    eq2 = (np.log(pi[0]) + y_e * np.log(theta[0, 0]) + y_n * np.log(theta[0, 1]) + y_c * np.log(theta[0, 2])) - (
+                np.log(pi[2]) + y_e * np.log(theta[2, 0]) + y_n * np.log(theta[2, 1]) + y_c * np.log(theta[2, 2]))
 
-    dict_enc = solve((eq1,eq2,eq3), (y_e,y_c,y_n))
+    dict_enc = solve((eq1, eq2, eq3), (y_e, y_c, y_n))
 
     # turn to coordinates
     point_ec = [dict_ec[y_e], dict_ec[y_n], dict_ec[y_c]]
@@ -290,6 +304,7 @@ def get_boundaries(pi, theta):
 
     return point_ec, point_en, point_nc, point_enc
 
+
 def col_to_label(tau):
     if pd.Series(np.argmax(tau, axis=1)).map(CB_color_cycle) == '#377eb8':
         return 'entailment'
@@ -297,8 +312,12 @@ def col_to_label(tau):
         return 'neutral'
     elif pd.Series(np.argmax(tau, axis=1)).map(CB_color_cycle) == '#4daf4a':
         return 'contradiction'
-def ternary_plot(Y, tau, pi, theta, scatter_visible = True, all=True, bounds=True, bounds_col=CB_color_cycle[7], alpha = 0.5):
-    figure, tax = base_plot()
+
+
+def ternary_plot(Y, tau, pi, theta, scatter_visible=True, all=True, bounds=True, bounds_col=CB_color_cycle[7],
+                 alpha=0.5, scale=100, fontsize=12, size=6, tick_fontsize=12, tick_offset=0.02, label_offset=0):
+    figure, tax = base_plot(scale=scale, fontsize=fontsize, size=size, tick_fontsize=tick_fontsize,
+                            tick_offset=tick_offset, label_offset=label_offset)
 
     if scatter_visible:
         if all:
@@ -306,7 +325,7 @@ def ternary_plot(Y, tau, pi, theta, scatter_visible = True, all=True, bounds=Tru
             tau_all = MultinomialExpectationMaximizer(K=3)._e_step(Y_all, pi=pi, theta=theta)
             tax.scatter(Y_all, c=pd.Series(np.argmax(tau_all, axis=1)).map(CB_color_cycle))
         else:
-            tax.scatter(Y, c=pd.Series(np.argmax(tau, axis=1)).map(CB_color_cycle), alpha = alpha)
+            tax.scatter(Y, c=pd.Series(np.argmax(tau, axis=1)).map(CB_color_cycle), alpha=alpha)
 
     if bounds:
         bounds = get_boundaries(pi=pi, theta=theta)
@@ -333,16 +352,17 @@ def multiple_boundaries(pi_list, theta_list, bounds_original):
 
     return figure, tax
 
+
 def multiple_boundaries_colored(pi_list, theta_list, bounds_original, cmap):
     figure, tax = base_plot()
-    cols = mcp.gen_color(cmap=cmap,n=len(pi_list)+10)
+    cols = mcp.gen_color(cmap=cmap, n=len(pi_list) + 10)
 
     for i in range(len(pi_list)):
         bounds = get_boundaries(pi=pi_list[i], theta=theta_list[i])
 
-        tax.line(bounds[0], bounds[3], color=cols[i+6])
-        tax.line(bounds[1], bounds[3], color=cols[i+6])
-        tax.line(bounds[2], bounds[3], color=cols[i+6])
+        tax.line(bounds[0], bounds[3], color=cols[i + 6])
+        tax.line(bounds[1], bounds[3], color=cols[i + 6])
+        tax.line(bounds[2], bounds[3], color=cols[i + 6])
 
     tax.line(bounds_original[0], bounds_original[3], color="red")
     tax.line(bounds_original[1], bounds_original[3], color="red")
