f3183f7c4a05a460cec8e76399f507b2c405c0c9
Cornelia Gruber <cornelia.gru@gmail.com>
2023-12-08 14:55:01 +0100

    do bootstrap for varying N and J

--- src/bootstrap_funcs.py
+++ src/bootstrap_funcs.py
@@ -53,24 +53,63 @@ def bootstrap(Y, N=None, K=3, B=50, seed=12):
 
     return {'pi': B_pi, 'theta': B_theta, 'tau': B_tau, 'sample': B_sample, 'bounds': B_bounds}
 
-# N, C = Y.shape
-# pi = np.zeros((B, K))
-# theta = np.zeros((B, K, C))
-# tau = np.zeros((B, N, K))
-#
-# for b in range(B):
-#     # Sample with replacement
-#     Y_b = np.zeros((N, C))
-#     for n in range(N):
-#         Y_b[n] = Y[random.randint(0, N - 1)]
-#
-#     # Run EM
-#     em = EM(K=K)
-#     _, pi_b, theta_b, tau_b = em.fit(Y_b)
-#
-#     # Save results
-#     pi[b] = pi_b
-#     theta[b] = theta_b
-#     tau[b] = tau_b
-#
-# return pi, theta, tau
+def freq_to_matrix(Y):
+    mat = np.empty((len(Y),100))
+    i=0
+    for obs in Y:
+        arr = np.empty(100, dtype=int)
+        current_index = 0
+        for category, count in enumerate(obs):
+            arr[current_index:count+current_index] = category
+            current_index += count
+        mat[i] = arr
+        i+=1
+    return mat
+
+def N_J_bootstrap_em(N_J_comb, snli_annotations, B=50, seed=12):
+    np.random.seed(seed)
+
+    N, J = N_J_comb
+
+    B_samples_N_J, B_pi_N_J, B_theta_N_J, B_tau_N_J, B_bounds_N_J = [], [], [], [], []
+
+    for b in range(B):
+        if b%10 == 0:
+            print('Bootstrap iteration: ', b)
+
+        if J < 100:
+            random_J = np.random.choice(100, size=J, replace=False)
+            annotations_J = snli_annotations[:,random_J].astype(int)
+            count_matrix = np.zeros((3, len(annotations_J)), dtype=int)
+
+            for row_idx in range(annotations_J.shape[0]):
+                row = annotations_J[row_idx]
+                unique, counts = np.unique(row, return_counts=True)
+                count_matrix[unique, row_idx] = counts
+
+            snli_one_hot_J = count_matrix.T
+
+        else:
+            snli_one_hot_J = snli_one_hot_arr
+
+        b_sample = np.array(random.choices(snli_one_hot_J, k=N))
+
+        try:
+            b_snli_em = run_em(b_sample, K=3)
+        except:
+            print('Error in EM')
+            continue
+
+        matched_index = np.argsort(np.argmax(b_snli_em[2], axis=1))
+        b_pi_matched = b_snli_em[1][matched_index]
+        b_theta_matched = b_snli_em[2][matched_index]
+        b_tau_matched = b_snli_em[3][:, matched_index]
+        b_bounds = get_boundaries(b_pi_matched, b_theta_matched)
+
+        B_samples_N_J.append(b_sample)
+        B_pi_N_J.append(b_pi_matched)
+        B_theta_N_J.append(b_theta_matched)
+        B_tau_N_J.append(b_tau_matched)
+        B_bounds_N_J.append(b_bounds)
+
+    return B_pi_N_J, B_theta_N_J, B_tau_N_J, B_samples_N_J, B_bounds_N_J, N_J_comb
